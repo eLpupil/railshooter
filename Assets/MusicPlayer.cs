aade68f7d84d322acab0bf2002e83d505529e124
JX <jxie1995@gmail.com>
2020-04-18 00:25:02 -0700

    1. Music Player gameObject and script 2. DontDestroyOnLoad (don't destroy gameObject upon scene change for continuous music)

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -8,15 +8,25 @@ public class MusicPlayer : MonoBehaviour
 
     [SerializeField] float loadSceneDelay = 5f;
 
+    
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         Invoke("LoadFirstScene", loadSceneDelay);
     }
 
+
     // Update is called once per frame
     private void LoadFirstScene()
     {
         SceneManager.LoadScene(1);
     }
+    
+    
 }
