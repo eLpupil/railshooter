08e39eaff9de06b57c7147dbfe04aaa45d92ac3f
JX <jxie1995@gmail.com>
2020-04-19 01:20:27 -0700

    1. C# script for ship 2. Cross platform input manager 3. Ship translational movement

---
+++ Assets/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 50f;
+    [Tooltip("In m")][SerializeField] float xRange = 20f;
+
+    [Tooltip("In ms^-1")][SerializeField] float ySpeed = 50f;
+    [Tooltip("In m")][SerializeField] float yRange = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        transform.localRotation = Quaternion.Euler(-30f, 30f, 0);
+    }
+
+    private void ProcessTranslation()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
