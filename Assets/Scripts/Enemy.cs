0e7ec95012691edc15149889a764c0972276b0f1
JX <jxie1995@gmail.com>
2020-04-22 14:39:36 -0700

    1. Scoreboard

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,19 +7,32 @@ public class Enemy : MonoBehaviour
 {
     [Tooltip("FX Prefab on enemy")] [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+
+    [SerializeField] int pointsOnHit = 100;
+    ScoreBoard scoreBoard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
+        scoreBoard.scoreHit(pointsOnHit);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
-        boxCollider.isTrigger = false;
-    }
+
 
 
     // Update is called once per frame
