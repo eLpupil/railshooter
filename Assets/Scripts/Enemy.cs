7f8dfbb3057a02f173dc9e2b64f69c1f4349f1ff
JX <jxie1995@gmail.com>
2020-04-21 23:11:14 -0700

    1. Making scripts add component (enemy.cs will add a non trigger box collider to ship gameobject on start) 2. Instantiating at Runtime (Enemy death FX prefab instance will be instantiated upon particle collision) 3. organize hierarchy pane by assigning parent transform of prefab instances to parent SerializeField

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,22 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [Tooltip("FX Prefab on enemy")] [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
     private void OnParticleCollision(GameObject other)
     {
-        print("particle collision with enemy " + gameObject.name);
-        //deathFX.SetActive(true);
+        print("particle collision");
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
     }
+
 
     // Update is called once per frame
     void Update()
