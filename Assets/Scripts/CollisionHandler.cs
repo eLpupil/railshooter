08b853d96e74456d20af6892df49bdf0daa3bd3a
JX <jxie1995@gmail.com>
2020-04-24 21:13:15 -0700

    1. Added debug key to disable player collisions 2. Added Text to Canvas

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -8,16 +8,32 @@ public class CollisionHandler : MonoBehaviour
     [Tooltip("In seconds")][SerializeField] float loadSceneDelay = 1.5f;
     [Tooltip("FX Prefab on player")][SerializeField] GameObject deathFX;
 
+    bool collisionDisabled = false;
+
+
+    private void Update()
+    {
+        ProcessDebugKey();
+    }
+
     private void OnTriggerEnter(Collider other)
+    {
+        if (collisionDisabled) { return; }
+
+        else
+        {
+            ProcessTriggerEvent(other);
+        }
+    }
+
+    private void ProcessTriggerEvent(Collider other)
     {
         switch (other.gameObject.tag)
         {
             case "Enemy":
-                print("Hit enemy ship");
                 StartDeathSequence();
                 break;
             default:
-                print("Player hit terrain");
                 StartDeathSequence();
                 break;
         }
@@ -25,13 +41,30 @@ public class CollisionHandler : MonoBehaviour
 
     private void StartDeathSequence()
     {
-        SendMessage("OnPlayerDeath");
-        deathFX.SetActive(true);
-        Invoke("LoadScene1", loadSceneDelay);
+        if (!Debug.isDebugBuild)
+            SendMessage("OnPlayerDeath");
+            deathFX.SetActive(true);
+            Invoke("LoadScene1", loadSceneDelay);
     }
 
     private void LoadScene1() //referenced in a string
     {
         SceneManager.LoadScene(1);
+    }
+
+    private void ProcessDebugKey()
+    {
+        if (Debug.isDebugBuild)
+        {
+            RespondToDebugKey();
+        }
+    }
+
+    private void RespondToDebugKey()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled;
+        }
     }
 }
